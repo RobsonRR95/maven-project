3c51bf52f6e1b6cbd6953a48ffab066e4c9d3f20
Robson Rodrigues <robson.r.r@hotmail.com>
2018-07-02 22:22:12 -0300

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -9,9 +9,12 @@ public class Greeter {
    * This is a constructor.
    */
   public Greeter() {
-
+    
   }
-
+  
+  public double maiorDouble(double v1, double v2){
+    return v1 > v2 ? v1 : v2;
+  }
   //TODO: Add javadoc comment
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
